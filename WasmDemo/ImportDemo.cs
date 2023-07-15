b5dca0765bc83398ee2f12cf4b5ee347ea8c2238
zxyao145 <zxyao145@gmail.com>
2023-07-15 23:28:30 +0800

    feat: using wat file

---
+++ WasmDemo/ImportDemo.cs
@@ -0,0 +1,41 @@
+﻿
+using WasmDemo;
+using Wasmtime;
+
+internal class ImportDemo
+{
+    public static void Run()
+    {
+
+        var wasmFile = "../../../../WasmLib/bin/Debug/net6.0/WasmLib.wasm";
+        var goWasmFile = "../../../../wasm-module/go-wasm-module/go-wasm-module.wasm";
+        var rustWasmFile = "../../../../wasm-module/rust-wasm-module/target/wasm32-wasi/release/rust_wasm_module.wasm";
+
+        using var wasmHelper = new WasmHelper();
+
+        wasmHelper.Linker.DefineFunction("env", "hello_from_env", () =>
+        {
+            Console.WriteLine("Hello from the Dotnet Host!");
+        });
+
+        using var goWasmModule = Module.FromFile(wasmHelper.Engine, goWasmFile);
+        using var wasmLibModule = Module.FromFile(wasmHelper.Engine, wasmFile);
+
+        wasmHelper.Linker.DefineModule(wasmHelper.Store, goWasmModule);
+
+        var goWasmIns = wasmHelper.GetInstance(goWasmModule);
+        var goMainFunc = goWasmIns.GetFunction("_start")!;
+        goMainFunc.Invoke();
+        var r = goWasmIns.GetFunction("main.add")?.Invoke(1, 3);
+
+        var wasmLibIns = wasmHelper.GetInstance(wasmLibModule);
+        var cSharpMainFunc = wasmLibIns.GetFunction("_start")!;
+        cSharpMainFunc.Invoke();
+
+
+        using var rustWasmModule = Module.FromFile(wasmHelper.Engine, rustWasmFile);
+        var rustWasmIns = wasmHelper.GetInstance(rustWasmModule);
+        var res = rustWasmIns.GetFunction("add")?.Invoke(4, 7);
+        Console.WriteLine(res);
+    }
+}
--- WasmDemo/Program.cs
+++ WasmDemo/Program.cs
@@ -1,42 +1,38 @@
-﻿
-using System.IO;
-using System.Reflection;
-using System.Text;
-using WasmDemo;
-using Wasmtime;
-using static System.Formats.Asn1.AsnWriter;
+﻿using WasmDemo;
 using Module = Wasmtime.Module;
 
 // ExportDemo.Run();
+// ImportDemo.Run();
 
-// using import in c#
-var wasmFile = "../../../../WasmLib/bin/Debug/net6.0/WasmLib.wasm";
-var goWasmFile = "../../../../wasm-module/go-wasm-module/go-wasm-module.wasm";
-var rustWasmFile = "../../../../wasm-module/rust-wasm-module/target/wasm32-wasi/release/rust_wasm_module.wasm";
+var watText = @"(module
+  (type (;0;) (func (param i32 i32) (result i32)))
+  (type (;1;) (func))
+  (func $add (type 0) (param i32 i32) (result i32)
+    local.get 1
+    local.get 0
+    i32.add)
+  (func $dummy (type 1))
+  (func $__wasm_call_dtors (type 1)
+    call $dummy
+    call $dummy)
+  (func $add.command_export (type 0) (param i32 i32) (result i32)
+    local.get 0
+    local.get 1
+    call $add
+    call $__wasm_call_dtors)
+  (table (;0;) 1 1 funcref)
+  (memory (;0;) 16)
+  (global $__stack_pointer (mut i32) (i32.const 1048576))
+  (export ""memory"" (memory 0))
+  (export ""add"" (func $add.command_export)))";
+
 
 using var wasmHelper = new WasmHelper();
 
-wasmHelper.Linker.DefineFunction("env", "hello_from_env", () =>
-{
-    Console.WriteLine("Hello from the Dotnet Host!");
-});
+using var wasmModule = Module.FromText(wasmHelper.Engine, "module-name", watText);
 
-using var goWasmModule = Module.FromFile(wasmHelper.Engine, goWasmFile);
-using var wasmLibModule = Module.FromFile(wasmHelper.Engine, wasmFile);
+wasmHelper.Linker.DefineModule(wasmHelper.Store, wasmModule);
 
-wasmHelper.Linker.DefineModule(wasmHelper.Store, goWasmModule);
-
-var goWasmIns = wasmHelper.GetInstance(goWasmModule);
-var goMainFunc = goWasmIns.GetFunction("_start")!;
-goMainFunc.Invoke();
-var r = goWasmIns.GetFunction("main.add")?.Invoke(1, 3);
-
-var wasmLibIns = wasmHelper.GetInstance(wasmLibModule);
-var cSharpMainFunc = wasmLibIns.GetFunction("_start")!;
-cSharpMainFunc.Invoke();
-
-
-using var rustWasmModule = Module.FromFile(wasmHelper.Engine, rustWasmFile);
-var rustWasmIns = wasmHelper.GetInstance(rustWasmModule);
-var res = rustWasmIns.GetFunction("add")?.Invoke(4, 7);
+var wasmIns = wasmHelper.GetInstance(wasmModule);
+var res = wasmIns.GetFunction("add")?.Invoke(4, 7);
 Console.WriteLine(res);
